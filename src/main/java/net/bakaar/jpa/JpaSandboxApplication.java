27df3516ecdc39d197415aeb4512dab2d6a9bf14
Stéphane Krattinger <stef.krattinger@gmail.com>
2022-10-30 20:31:50 +0100

    minimum spring boot module

---
+++ src/main/java/net/bakaar/jpa/JpaSandboxApplication.java
@@ -0,0 +1,13 @@
+package net.bakaar.jpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaSandboxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaSandboxApplication.class, args);
+    }
+
+}
